804b7e2e5569359f03e4308b7705d03451a1e7af
Zach Petch <zachpetch@gmail.com>
2022-03-30 00:04:10 -0700

    Memory allocation for strings.
    Today I learned that strings, in C, are immutable.

--- strmath.c
+++ strmath.c
@@ -15,22 +15,18 @@
  */
 char* strrev(char *str)
 {
-    if (!str || ! *str)
-        return str;
-
-    int i = strlen(str) - 1, j = 0;
-
-    char ch;
-    while (i > j)
-    {
-        ch = str[i];
-        str[i] = str[j];
-        str[j] = ch;
-        i--;
-        j++;
-    }
-
+  if (!str || ! *str)
     return str;
+
+  char *newstr = malloc(strlen(str));
+  int i = strlen(str) - 1;
+
+  while (i >= 0)
+  {
+    newstr = strncat(newstr, &str[i--], 1);
+  }
+
+  return newstr;
 }
 
 /**
@@ -38,8 +34,7 @@ char* strrev(char *str)
  */
 static char* _add(char* str1, char* str2)
 {
-  char *a, *b;
-  char *c = "";
+  char *a, *b, *c;
   int lengthDiff;
 
   // Set the longest string to a, and the shortest to b.
@@ -48,21 +43,27 @@ static char* _add(char* str1, char* str2)
     a = strrev(str2);
     b = strrev(str1);
     lengthDiff = strlen(str2) - strlen(str1);
+    c = malloc(strlen(str2) + 1);
   }
   else
   {
     a = strrev(str1);
     b = strrev(str2);
     lengthDiff = strlen(str1) - strlen(str2);
+    c = malloc(strlen(str1) + 1);
   }
 
   // Add the two string integers together.
   int carry = 0;
   int sum, i;
-  char *t = malloc(sizeof(char) * (strlen(a) + 1));
+  char *t = malloc(1);
   for (i = 0; i < strlen(b); i++)
   {
+    // TODO: DELETE THIS TEST PRINT
+    printf("i = %d; a[i] = %d; b[i] = %d; carry = %d;\n", i, a[i], b[i], carry);
     sum = atoi(&a[i]) + atoi(&b[i]) + carry;
+    // TODO: DELETE THIS TEST PRINT
+    printf("sum = %d;\n", sum);
     if (sum > 9)
     {
       carry = 1;
@@ -73,8 +74,10 @@ static char* _add(char* str1, char* str2)
       carry = 0;
     }
 
-    sprintf(t, "%c", sum);
+    sprintf(t, "%d", sum);
     strncat(c, t, 1);
+    // TODO: DELETE THIS TEST PRINT
+    printf("so far c is %s\n", c);
   }
 
   if (carry > 0)
@@ -94,14 +97,14 @@ static char* _add(char* str1, char* str2)
           carry = 0;
         }
 
-        sprintf(t, "%c", sum);
+        sprintf(t, "%d", sum);
         strncat(c, t, 1);
         i++;
       }
 
       if (carry > 0)
       {
-        sprintf(t, "%c", carry);
+        sprintf(t, "%d", carry);
         strncat(c, t, 1);
       }
 
@@ -117,8 +120,7 @@ static char* _add(char* str1, char* str2)
     {
       // If there is still something being carried over, but the two strings are the same length,
       // then we simply add the carried vallue to the end of the string.
-
-      sprintf(t, "%c", carry);
+      sprintf(t, "%d", carry);
       strncat(c, t, 1);
     }
   }
@@ -143,7 +145,7 @@ char* strmath_sum(char* a, char* b)
     char* unsignedB = b + 1;
     char* sum = _add(unsignedA, unsignedB);
     char* c = (char*) malloc(strlen(sum) + 1);
-    c[0] = '-';
+    c[0] = '-'; //segfault, can't edit contents of a string
     strcat(c,sum);
 
     return c;
@@ -177,6 +179,18 @@ int main(int argc, char *argv[])
   printf("test sum of two positive integers\n");
   a = "7";
   b = "5";
+  c = strmath_sum(a, b);
+  printf("%s + %s = %s\n", a, b, c);
+
+  printf("test sum of two bigger positive integers\n");
+  a = "712";
+  b = "511";
+  c = strmath_sum(a, b);
+  printf("%s + %s = %s\n", a, b, c);
+
+  printf("test sum of two asymetrical positive integers\n");
+  a = "999";
+  b = "999999";
   c = strmath_sum(a, b);
   printf("%s + %s = %s\n", a, b, c);
 
